431f3d28f3d8f648a0df406ba862dec8f11838bc
cfx <jozef@vzaar.com>
2017-05-30 21:11:05 +0200

    run specs in emacs term

--- rsp.el
+++ rsp.el
@@ -1,8 +1,10 @@
-(defvar *rsp-tmux-cmd* "tmux send-keys -t 1 'clear && docker-compose exec %s rspec %s' Enter")
+;;(setq *rsp-tmux-cmd* "clear && docker-compose exec %s rspec %s")
+(setq *rsp-tmux-cmd* "clear && docker-compose exec %s rspec %s")
 
 (defun rsp ()
   (interactive)
-  (shell-command (format *rsp-tmux-cmd* (dev-repo-name) (rsp-path))))
+  (rsp-send-cmd (format *rsp-tmux-cmd* (dev-repo-name) (rsp-path))))
+
 
 (defun rsp-block ()
   (interactive)
@@ -10,6 +12,10 @@
                       ":"
                       (number-to-string (1+ (count-lines 1 (point)))))))
     (shell-command (format *rsp-tmux-cmd* (dev-repo-name) path))))
+
+(defun rsp-tool ()
+  (interactive)
+  (call-process (format "docker exec -it %s which rspec > /dev/null" (dev-repo-name))))
 
 (defun rsp-open ()
   (interactive)
@@ -41,3 +47,20 @@
 (defun rsp-paths (repo-path filepath)
   (let ((re (format "\\(%s\/app\\|%s\\|\.rb\\)" repo-path repo-path)))
     (split-string filepath re)))
+
+(defun rsp-send-cmd (cmd &optional _num)
+  (interactive)
+  (let ((buf "rsp"))
+    (ansi-term (getenv "SHELL") buf)
+    (with-current-buffer (concat "*" buf "*")
+      (lexical-let ((p (get-buffer-process (current-buffer))))
+        (local-set-key (kbd "k")
+                       (lambda ()
+                         (interactive)
+                         (delete-process p)
+                         (kill-this-buffer)
+                         (local-unset-key (kbd "k"))))
+
+
+        (goto-char (process-mark p))
+        (apply comint-input-sender (list p cmd))))))

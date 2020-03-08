32cbe24199aa399e531cb5bfceb23bc348ceafe5
Jozef Chraplewski <jozef@blockfi.com>
2020-03-08 01:32:07 +0100

    wip

--- rsp.el
+++ rsp.el
@@ -6,6 +6,7 @@
 (setq *rsp-test-cmd* "a=$(if docker-compose exec %s which rspec > /dev/null;
 then echo 'bundle exec rspec'; else echo ruby; fi); clear; docker-compose exec %s $a %s")
 
+
 (define-minor-mode rsp-minor-mode
   :init-value nil
   :keymap (let ((map (make-sparse-keymap)))
@@ -21,52 +22,85 @@ then echo 'bundle exec rspec'; else echo ruby; fi); clear; docker-compose exec %
     (rsp-send-cmd (format *rsp-test-cmd*
                           container-name
                           container-name
-                          (rsp-path)))))
+                          (rsp-path (rsp-cfg))))))
 
 (defun rsp-block ()
   (interactive)
-  (let ((path (concat (rsp-path)
+    (message (rsp-path (rsp-cfg)))
+  (let ((path (concat (rsp-path (rsp-cfg))
                       ":"
-                      (number-to-string (1+ (count-lines 1 (point))))))
-        (container-name (dev-repo-name)))
+                      (number-to-string (1+ (count-lines 1 (point)))))))
 
-    (rsp-send-cmd (format *rsp-test-cmd*
-                          container-name
-                          container-name
-                          path))))
+    (rsp-send-cmd (funcall (rsp-exec-cmd cfg) path))))
+
+(defun rsp-test-dir    (cfg) (car cfg))
+(defun rsp-test-suffix (cfg) (cadr cfg))
+(defun rsp-file-suffix (cfg) (caddr cfg))
+(defun rsp-path-regex  (cfg) (cadddr cfg))
+
+(defun rsp-exec-cmd (cfg)
+  (cadddr (cdr cfg)))
+
+
+(defun rsp-cfg ()
+  (let ((suffix (cadr (split-string (buffer-name) "\\."))))
+    (if (equal suffix "rb")
+	(let* ((test-dir "/spec" )
+	       (test-suffix "_spec.rb")
+	       (test-file-suffix ".rb")
+	       (path-regex "\\(%s\/app\\|%s\\|\.rb\\)")
+	       (exec-cmd (lexical-let ((container-name (dev-repo-name)))
+			   (lambda (path)
+			     (format *rsp-test-cmd*
+				     container-name
+				     container-name
+				     path)))))
+
+	  (list test-dir test-suffix test-file-suffix path-regex exec-cmd))
+
+	(let* ((test-dir "/test")
+	       (test-suffix "_test.exs")
+	       (test-file-suffix ".exs")
+	       (path-regex "\\(%s\/lib\\|%s\\|\.ex\\)")
+	       (exec-cmd (lambda (path)
+			   (format "mix test %s" path))))
+
+	  (list test-dir test-suffix test-file-suffix path-regex exec-cmd)))))
 
 (defun rsp-open ()
   (interactive)
   (let* ((buf (buffer-file-name))
+	 (cfg (rsp-cfg))
          (repo-path (dev-repo-path))
-         (paths (rsp-paths repo-path buf)))
+         (paths (rsp-paths repo-path buf cfg)))
 
-    (unless (string-match "_spec.rb" buf)
+    (unless (string-match (rsp-test-suffix cfg) buf)
       (if (= (length (window-list)) 1)
 	  (split-window-right))
       (other-window 1)
+
       (find-file (mapconcat 'identity
                             (list (car paths)
                                   repo-path
-                                  "/spec"
+                                  (rsp-test-dir cfg)
                                   (cadr paths)
-                                  "_spec.rb") "")))))
+                                  (rsp-test-suffix cfg)) "")))))
 
 (defun rsp-all ()
   (interactive)
   (rsp-send-cmd (format *rsp-test-all-cmd* (dev-repo-name))))
 
-(defun rsp-path ()
+(defun rsp-path (cfg)
   (let* ((buf (buffer-file-name))
          (filename (car (last (split-string buf "/"))))
-         (paths (rsp-paths (dev-repo-path) buf)))
+         (paths (rsp-paths (dev-repo-path) buf cfg)))
 
-    (if (string-match "_spec.rb" filename)
-        (mapconcat 'identity (list "." (cadr paths) ".rb") "")
-      (mapconcat 'identity (list "spec" (cadr paths) "_spec.rb") ""))))
+    (if (string-match (rsp-test-suffix) filename)
+        (mapconcat 'identity (list "." (cadr paths) (rsp-file-suffix)) "")
+      (mapconcat 'identity (list rsp-test-dir (cadr paths) (rsp-test-suffix)) ""))))
 
-(defun rsp-paths (repo-path filepath)
-  (let ((re (format "\\(%s\/app\\|%s\\|\.rb\\)" repo-path repo-path)))
+(defun rsp-paths (repo-path filepath cfg)
+  (let ((re (format (rsp-path-regex cfg) repo-path repo-path)))
     (split-string filepath re)))
 
 (defun rsp-send-cmd (cmd)

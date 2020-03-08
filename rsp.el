285d55d576de65eb388a6024ccd6ac3bc173043f
Jozef Chraplewski <jozef@blockfi.com>
2020-03-08 16:20:48 +0100

    Add support for elixir, remove rsp-all and docker

--- rsp.el
+++ rsp.el
@@ -1,37 +1,20 @@
 (require 'comint)
 (require 'dev.el)
 
-(setq *rsp-proc* nil)
-(setq *rsp-test-all-cmd* "docker-compose exec %s rake test")
-(setq *rsp-test-cmd* "a=$(if docker-compose exec %s which rspec > /dev/null;
-then echo 'bundle exec rspec'; else echo ruby; fi); clear; docker-compose exec %s $a %s")
-
-
-(define-minor-mode rsp-minor-mode
-  :init-value nil
-  :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "\C-v\C-k") (lambda ()
-                                        (interactive)
-                                        (delete-process *rsp-proc*)
-                                        (kill-this-buffer)))
-            map))
-
 (defun rsp ()
   (interactive)
-  (let ((container-name (dev-repo-name)))
-    (rsp-send-cmd (format *rsp-test-cmd*
-                          container-name
-                          container-name
-                          (rsp-path (rsp-cfg))))))
+  (let* ((cfg (rsp-cfg))
+	 (path (rsp-path cfg)))
+    (dev-shell-cmd (funcall (rsp-exec-cmd cfg) path))))
 
 (defun rsp-block ()
   (interactive)
-    (message (rsp-path (rsp-cfg)))
-  (let ((path (concat (rsp-path (rsp-cfg))
-                      ":"
-                      (number-to-string (1+ (count-lines 1 (point)))))))
+  (let* ((cfg (rsp-cfg))
+	 (path (concat (rsp-path cfg)
+		       ":"
+		       (number-to-string (1+ (count-lines 1 (point)))))))
 
-    (rsp-send-cmd (funcall (rsp-exec-cmd cfg) path))))
+    (dev-shell-cmd (funcall (rsp-exec-cmd cfg) path))))
 
 (defun rsp-test-dir    (cfg) (car cfg))
 (defun rsp-test-suffix (cfg) (cadr cfg))
@@ -41,20 +24,17 @@ then echo 'bundle exec rspec'; else echo ruby; fi); clear; docker-compose exec %
 (defun rsp-exec-cmd (cfg)
   (cadddr (cdr cfg)))
 
-
 (defun rsp-cfg ()
-  (let ((suffix (cadr (split-string (buffer-name) "\\."))))
+  (lexical-let ((suffix (cadr (split-string (buffer-name) "\\.")))
+		(repo-name (dev-repo-name)))
+
     (if (equal suffix "rb")
 	(let* ((test-dir "/spec" )
 	       (test-suffix "_spec.rb")
 	       (test-file-suffix ".rb")
 	       (path-regex "\\(%s\/app\\|%s\\|\.rb\\)")
-	       (exec-cmd (lexical-let ((container-name (dev-repo-name)))
-			   (lambda (path)
-			     (format *rsp-test-cmd*
-				     container-name
-				     container-name
-				     path)))))
+	       (exec-cmd (lambda (path)
+			   (format "tmux send-keys -t \"0:%s.0\" \"bundle exec rspec %s\" Enter" repo-name path))))
 
 	  (list test-dir test-suffix test-file-suffix path-regex exec-cmd))
 
@@ -63,7 +43,7 @@ then echo 'bundle exec rspec'; else echo ruby; fi); clear; docker-compose exec %
 	       (test-file-suffix ".exs")
 	       (path-regex "\\(%s\/lib\\|%s\\|\.ex\\)")
 	       (exec-cmd (lambda (path)
-			   (format "mix test %s" path))))
+			   (format "tmux send-keys -t \"0:%s.0\" \"mix test %s\" Enter" repo-name path))))
 
 	  (list test-dir test-suffix test-file-suffix path-regex exec-cmd)))))
 
@@ -86,35 +66,18 @@ then echo 'bundle exec rspec'; else echo ruby; fi); clear; docker-compose exec %
                                   (cadr paths)
                                   (rsp-test-suffix cfg)) "")))))
 
-(defun rsp-all ()
-  (interactive)
-  (rsp-send-cmd (format *rsp-test-all-cmd* (dev-repo-name))))
 
 (defun rsp-path (cfg)
   (let* ((buf (buffer-file-name))
          (filename (car (last (split-string buf "/"))))
          (paths (rsp-paths (dev-repo-path) buf cfg)))
 
-    (if (string-match (rsp-test-suffix) filename)
-        (mapconcat 'identity (list "." (cadr paths) (rsp-file-suffix)) "")
-      (mapconcat 'identity (list rsp-test-dir (cadr paths) (rsp-test-suffix)) ""))))
+    (if (string-match (rsp-test-suffix cfg) filename)
+        (mapconcat 'identity (list "." (cadr paths) (rsp-file-suffix cfg)) "")
+      (mapconcat 'identity (list rsp-test-dir (cadr paths) (rsp-test-suffix cfg)) ""))))
 
 (defun rsp-paths (repo-path filepath cfg)
   (let ((re (format (rsp-path-regex cfg) repo-path repo-path)))
     (split-string filepath re)))
-
-(defun rsp-send-cmd (cmd)
-  (let* ((buf "rsp")
-         (buf-term (concat "*" buf "*")))
-
-    (if (get-buffer buf-term)
-        (switch-to-buffer buf-term)
-      (ansi-term (getenv "SHELL") buf))
-
-    (with-current-buffer buf-term
-      (rsp-minor-mode 1)
-      (setq *rsp-proc* (get-buffer-process (current-buffer)))
-      (goto-char (process-mark *rsp-proc*))
-      (apply comint-input-sender (list *rsp-proc* cmd)))))
 
 (provide 'rsp)

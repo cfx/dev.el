fa05668506e6bf97761c36ec06b64a443452b059
Jozef Chraplewski <jozef@blockfi.com>
2020-03-08 16:23:47 +0100

    Rename rsp to tt

--- tt.el
+++ tt.el
@@ -1,30 +1,30 @@
 (require 'comint)
 (require 'dev.el)
 
-(defun rsp ()
+(defun tt ()
   (interactive)
-  (let* ((cfg (rsp-cfg))
-	 (path (rsp-path cfg)))
-    (dev-shell-cmd (funcall (rsp-exec-cmd cfg) path))))
+  (let* ((cfg (tt-cfg))
+	 (path (tt-path cfg)))
+    (dev-shell-cmd (funcall (tt-exec-cmd cfg) path))))
 
-(defun rsp-block ()
+(defun tt-block ()
   (interactive)
-  (let* ((cfg (rsp-cfg))
-	 (path (concat (rsp-path cfg)
+  (let* ((cfg (tt-cfg))
+	 (path (concat (tt-path cfg)
 		       ":"
 		       (number-to-string (1+ (count-lines 1 (point)))))))
 
-    (dev-shell-cmd (funcall (rsp-exec-cmd cfg) path))))
+    (dev-shell-cmd (funcall (tt-exec-cmd cfg) path))))
 
-(defun rsp-test-dir    (cfg) (car cfg))
-(defun rsp-test-suffix (cfg) (cadr cfg))
-(defun rsp-file-suffix (cfg) (caddr cfg))
-(defun rsp-path-regex  (cfg) (cadddr cfg))
+(defun tt-test-dir    (cfg) (car cfg))
+(defun tt-test-suffix (cfg) (cadr cfg))
+(defun tt-file-suffix (cfg) (caddr cfg))
+(defun tt-path-regex  (cfg) (cadddr cfg))
 
-(defun rsp-exec-cmd (cfg)
+(defun tt-exec-cmd (cfg)
   (cadddr (cdr cfg)))
 
-(defun rsp-cfg ()
+(defun tt-cfg ()
   (lexical-let ((suffix (cadr (split-string (buffer-name) "\\.")))
 		(repo-name (dev-repo-name)))
 
@@ -34,7 +34,7 @@
 	       (test-file-suffix ".rb")
 	       (path-regex "\\(%s\/app\\|%s\\|\.rb\\)")
 	       (exec-cmd (lambda (path)
-			   (format "tmux send-keys -t \"0:%s.0\" \"bundle exec rspec %s\" Enter" repo-name path))))
+			   (format "tmux send-keys -t \"0:%s.0\" \"bundle exec ttec %s\" Enter" repo-name path))))
 
 	  (list test-dir test-suffix test-file-suffix path-regex exec-cmd))
 
@@ -47,14 +47,14 @@
 
 	  (list test-dir test-suffix test-file-suffix path-regex exec-cmd)))))
 
-(defun rsp-open ()
+(defun tt-open ()
   (interactive)
   (let* ((buf (buffer-file-name))
-	 (cfg (rsp-cfg))
+	 (cfg (tt-cfg))
          (repo-path (dev-repo-path))
-         (paths (rsp-paths repo-path buf cfg)))
+         (paths (tt-paths repo-path buf cfg)))
 
-    (unless (string-match (rsp-test-suffix cfg) buf)
+    (unless (string-match (tt-test-suffix cfg) buf)
       (if (= (length (window-list)) 1)
 	  (split-window-right))
       (other-window 1)
@@ -62,22 +62,22 @@
       (find-file (mapconcat 'identity
                             (list (car paths)
                                   repo-path
-                                  (rsp-test-dir cfg)
+                                  (tt-test-dir cfg)
                                   (cadr paths)
-                                  (rsp-test-suffix cfg)) "")))))
+                                  (tt-test-suffix cfg)) "")))))
 
 
-(defun rsp-path (cfg)
+(defun tt-path (cfg)
   (let* ((buf (buffer-file-name))
          (filename (car (last (split-string buf "/"))))
-         (paths (rsp-paths (dev-repo-path) buf cfg)))
+         (paths (tt-paths (dev-repo-path) buf cfg)))
 
-    (if (string-match (rsp-test-suffix cfg) filename)
-        (mapconcat 'identity (list "." (cadr paths) (rsp-file-suffix cfg)) "")
-      (mapconcat 'identity (list rsp-test-dir (cadr paths) (rsp-test-suffix cfg)) ""))))
+    (if (string-match (tt-test-suffix cfg) filename)
+        (mapconcat 'identity (list "." (cadr paths) (tt-file-suffix cfg)) "")
+      (mapconcat 'identity (list tt-test-dir (cadr paths) (tt-test-suffix cfg)) ""))))
 
-(defun rsp-paths (repo-path filepath cfg)
-  (let ((re (format (rsp-path-regex cfg) repo-path repo-path)))
+(defun tt-paths (repo-path filepath cfg)
+  (let ((re (format (tt-path-regex cfg) repo-path repo-path)))
     (split-string filepath re)))
 
-(provide 'rsp)
+(provide 'tt)
